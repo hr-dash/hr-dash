8860648e558275f2fa02d2c9f5598a400fb8a7fb
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2015-10-06 22:32:39 +0900

    Fix rubocop

--- spec/requests/sample_spec.rb
+++ spec/requests/sample_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe SampleController, type: :request do
-  describe "GET /sample" do
+  describe 'GET /sample' do
     before { get sample_path }
     it { expect(response).to be_success }
   end
