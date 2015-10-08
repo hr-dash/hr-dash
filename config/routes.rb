cd738448d053e9ed112df0d8fa0bca0fb6d63f84
“masataka.ohara” <1989.oxhara@gmail.com>
2015-10-08 22:02:10 +0900

    devise 一部修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'home/index'
-
-  get 'home/show'
 
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
@@ -20,8 +17,6 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-
-  root to: "home#index"
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
