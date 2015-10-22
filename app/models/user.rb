5e3caee0224d61218e319d0567ff71bc6e7b3a37
“masataka.ohara” <1989.oxhara@gmail.com>
2015-10-22 20:23:54 +0900

    user model からdevise optionのregistable を削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,6 @@ class User < ActiveRecord::Base
   validates :beginner_flg, inclusion: { in: [true, false] }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
 end
