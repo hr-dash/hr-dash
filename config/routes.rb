fd4ad5e04efffbe08b1874b309cc21a55883b5ab
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2015-10-25 18:07:01 +0900

    Add sample profile page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       get 'login'
       get 'monthly_report'
       get 'monthly_report_new'
+      get 'profile'
     end
   end
   # The priority is based upon order of creation: first created -> highest priority.
