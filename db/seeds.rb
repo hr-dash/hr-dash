d9b4e5274e528f301bcd517b7704c095a92058c6
shuta tezuka <deveenjhalf7@gmail.com>
2015-10-29 20:47:19 +0900

    update mailstring

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
-AdminUser.create(email: 'test@rakus.co.jp', password: 'test2000')
-AdminUser.create(email: 'test2@rakus.co.jp', password: 'test2000')
+AdminUser.create(email: 'test@develop.co.jp', password: 'test2000')
+AdminUser.create(email: 'test2@develop.co.jp', password: 'test2000')
