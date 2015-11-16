b4e0729952217690a4ba1fa6d85379da30855ad8
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2015-11-16 20:13:17 +0900

    Fix user factories
    > NOTE: While Faker generates data at random, returned values are not guaranteed to be unique.
    https://github.com/stympy/faker

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,8 +2,8 @@ FactoryGirl.define do
   factory :user do
     name Faker::Name.name
     association :group
-    employee_code Faker::Number.number(10)
-    email Faker::Internet.email
+    sequence(:employee_code) { |i| "#{i}#{Faker::Number.number(10)}" }
+    sequence(:email) { |i| "#{i}#{Faker::Internet.email}" }
     password Faker::Internet.password(8)
     entry_date Faker::Date.between(3.years.ago, Date.today)
     beginner_flg { [true, false].sample }
