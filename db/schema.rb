d1a6ebe7048de888cb840c2c3610b19563957f3e
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2015-11-23 17:50:16 +0900

    Add status to Tag

--- db/schema.rb
+++ db/schema.rb
@@ -103,10 +103,13 @@ ActiveRecord::Schema.define(version: 20151220122954) do
   end
 
   create_table "tags", force: :cascade do |t|
+    t.integer  "status",     null: false
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", using: :btree
 
   create_table "user_profiles", force: :cascade do |t|
     t.integer  "user_id"
