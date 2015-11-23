46ac1b8d1c861f5bbb5a8f8a3b88afb006381abb
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2015-11-23 17:52:22 +0900

    Fix factories

--- spec/factories/tag.rb
+++ spec/factories/tag.rb
@@ -1,5 +1,14 @@
 FactoryGirl.define do
   factory :tag do
+    status Tag.statuses[:fixed]
     name { Faker::Lorem.word }
+
+    trait :unfixed do
+      status Tag.statuses[:unfixed]
+    end
+
+    trait :fixed do
+      status Tag.statuses[:fixed]
+    end
   end
 end
