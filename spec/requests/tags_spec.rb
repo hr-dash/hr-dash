6729f416f5bc97129b84f778a33f773c1c26c220
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2015-11-23 17:53:01 +0900

    Fix spec for tag

--- spec/requests/tags_spec.rb
+++ spec/requests/tags_spec.rb
@@ -1,9 +1,20 @@
 describe TagsController, type: :request do
   describe 'GET /tags' do
-    let!(:tags) { create_list(:tag, 3) }
-    before { get tags_path, nil, headers_for_json }
+    describe 'return fixed tags' do
+      let!(:tags) { create_list(:tag, 3, :fixed) }
+      before { get tags_path, nil, headers_for_json }
 
-    it { expect(response).to be_success }
-    it { expect(JSON.parse(response.body).size).to eq 3 } 
+      it { expect(response).to be_success }
+      it { expect(JSON.parse(response.body).size).to eq 3 }
+    end
+
+    describe 'return only fixed tag' do
+      let!(:fixed) { create(:tag, :fixed) }
+      let!(:unfixed) { create(:tag, :unfixed) }
+      before { get tags_path, nil, headers_for_json }
+
+      it { expect(JSON.parse(response.body).size).to eq 1 }
+      it { expect(JSON.parse(response.body).first["name"]).to eq fixed.name }
+    end
   end
 end
