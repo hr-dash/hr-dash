6e440123e435749a8a9fc4ce889f4aff36f09472
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2015-11-25 23:39:24 +0900

    Change seed to only create user

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,6 @@ User.destroy_all
 MonthlyReport.destroy_all
 Tag.destroy_all
 
-AdminUser.create(email: 'test@develop.co.jp', password: 'test2000')
-AdminUser.create(email: 'test2@develop.co.jp', password: 'test2000')
+FactoryGirl.create(:user, email: 'test@example.com', password: 'password')
 
 5.times { FactoryGirl.create(:monthly_report_tag) }
