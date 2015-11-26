7f8231ba0c4b209e8afa558d89bd0c1de01bb707
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2015-11-26 23:26:55 +0900

    Set defalt locale to ja

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module Dash
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :ja
 
     # Add custom validators path
     config.autoload_paths += Dir["#{config.root}/app/validators"]
