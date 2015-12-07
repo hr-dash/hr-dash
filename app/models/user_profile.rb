a3e5b8d78dfef268298d4bd4bb94bb19a7a66b03
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2015-12-07 22:51:09 +0900

    Add validations for user_profile

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,6 +1,9 @@
 class UserProfile < ActiveRecord::Base
   belongs_to :user
 
+  validates :gender, presence: true
+  validates :blood_type, presence: true
+
   enum gender: { gender_hidden: 0, male: 1, female: 2 }
   enum blood_type: { blood_hidden: 0, a: 1, b: 2, ab: 3, o: 4 }
 end
