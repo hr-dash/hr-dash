d3232152cfd56e104001412bd9f18640d7e8c0c8
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2015-12-07 22:47:57 +0900

    Add model, migration for UserProfile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150928132655) do
+ActiveRecord::Schema.define(version: 20151207132531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,18 @@ ActiveRecord::Schema.define(version: 20150928132655) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_profiles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "self_introduction"
+    t.integer  "gender",            default: 0, null: false
+    t.integer  "blood_type",        default: 0, null: false
+    t.date     "birthday"
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+  end
+
+  add_index "user_profiles", ["user_id"], name: "index_user_profiles_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.integer  "group_id"
@@ -119,7 +131,9 @@ ActiveRecord::Schema.define(version: 20150928132655) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["employee_code"], name: "index_users_on_employee_code", unique: true, using: :btree
   add_index "users", ["group_id"], name: "index_users_on_group_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "user_profiles", "users"
 end
