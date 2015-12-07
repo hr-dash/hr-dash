b6afd948d132bc9a6f10d2127bc4121e35a09d81
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2015-12-07 22:53:34 +0900

    Add annotate

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: user_profiles
+#
+#  id                :integer          not null, primary key
+#  user_id           :integer
+#  self_introduction :text
+#  gender            :integer          default(0), not null
+#  blood_type        :integer          default(0), not null
+#  birthday          :date
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+
 class UserProfile < ActiveRecord::Base
   belongs_to :user
 
