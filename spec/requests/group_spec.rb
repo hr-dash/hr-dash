43fc2ae31fd39ce07785bb6995d1e212d4f1e268
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2015-12-16 21:39:26 +0900

    Add request spec for groups

---
+++ spec/requests/group_spec.rb
@@ -0,0 +1,12 @@
+describe GroupsController, type: :request do
+  before { login }
+
+  describe '#index GET groups' do
+    let!(:user) { create(:user) }
+    before { get groups_path }
+
+    it { expect(response).to have_http_status :success }
+    it { expect(response.body).to match user.name }
+    it { expect(response.body).to match user.group.name }
+  end
+end
