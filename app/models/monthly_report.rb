b4dd8c641f34819b5c3153bad4207b07d4f016dc
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2015-12-20 21:01:18 +0900

    Redefine monthly_report model
    - used_technology replaced with monthly_report_tags
    - rename responsible business with working process relations
    - replace year, month with target_month as datetime

--- app/models/monthly_report.rb
+++ app/models/monthly_report.rb
@@ -2,18 +2,16 @@
 #
 # Table name: monthly_reports
 #
-#  id                   :integer          not null, primary key
-#  user_id              :integer          not null
-#  project_summary      :text
-#  used_technology      :text
-#  responsible_business :text
-#  business_content     :text
-#  looking_back         :text
-#  next_month_goals     :text
-#  month                :integer          not null
-#  year                 :integer          not null
-#  created_at           :datetime         not null
-#  updated_at           :datetime         not null
+#  id               :integer          not null, primary key
+#  user_id          :integer          not null
+#  target_month     :datetime         not null
+#  project_summary  :text
+#  working_process  :integer          is an Array
+#  business_content :text
+#  looking_back     :text
+#  next_month_goals :text
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
 #
 
 class MonthlyReport < ActiveRecord::Base
@@ -21,12 +19,17 @@ class MonthlyReport < ActiveRecord::Base
   has_many :monthly_report_comments, dependent: :destroy
   has_many :monthly_report_tags, dependent: :destroy
   has_many :tags, through: :monthly_report_tags
+  has_many :monthly_working_processes, dependent: :destroy
 
   validates :user_id, numericality: { only_integer: true }, presence: true
-  validates :month, numericality: { only_integer: true },
-                    inclusion: { in: 1..12 },
-                    presence: true
-  validates :year, numericality: { only_integer: true,
-                                   greater_than_or_equal_to: 2000 },
-                   presence: true
+  validates :target_month, presence: true
+  validate :is_target_beginning_of_month
+
+  private
+
+  def is_target_beginning_of_month
+    return if target_month.blank?
+    return if target_month == target_month.beginning_of_month
+    errors.add :target_month, "invalid target month"
+  end
 end
