8683979c57da8b95457e2746e1eed5c9564a0d2d
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2015-12-20 20:13:40 +0900

    Add monthly_report new

--- app/controllers/monthly_reports_controller.rb
+++ app/controllers/monthly_reports_controller.rb
@@ -5,4 +5,8 @@ class MonthlyReportsController < ApplicationController
   def show
     @monthly_report = MonthlyReport.find(params[:id])
   end
+
+  def new
+    @monthly_report = MonthlyReport.new
+  end
 end
