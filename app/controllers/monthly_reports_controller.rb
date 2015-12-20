8630d97bc7a8d5c048ee9d518a110b17507ec7c6
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2015-12-20 23:51:58 +0900

    Edit new, create for monthly_report, except for tags and working_processes

--- app/controllers/monthly_reports_controller.rb
+++ app/controllers/monthly_reports_controller.rb
@@ -7,6 +7,31 @@ class MonthlyReportsController < ApplicationController
   end
 
   def new
-    @monthly_report = MonthlyReport.new
+    target_month = Time.current.beginning_of_month
+    @monthly_report = MonthlyReport.new(target_month: target_month)
+  end
+
+  def create
+    @monthly_report = MonthlyReport.new(permitted_params) do |report|
+      report.user = current_user
+    end
+
+    if @monthly_report.save
+      redirect_to @monthly_report
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def permitted_params
+    params.require(:monthly_report).permit(
+      :target_month,
+      :project_summary,
+      :business_content,
+      :looking_back,
+      :next_month_goals,
+    )
   end
 end
