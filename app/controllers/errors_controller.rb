8b525902fdc85542bd544ea75d05f8145bad340f
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2015-12-23 15:07:15 +0900

    Fix UncommunicativeMethodName of reek alert

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,18 +1,18 @@
 class ErrorsController < ActionController::Base
   layout 'error'
 
-  rescue_from StandardError, with: :render_500
-  rescue_from ActiveRecord::RecordNotFound, with: :render_404
-  rescue_from ActionController::RoutingError, with: :render_404
+  rescue_from StandardError, with: :render_internal_server_error
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActionController::RoutingError, with: :render_not_found
 
-  def render_404(exception = nil)
+  def render_not_found(exception = nil)
     if exception
       logger.info "Rendering 404 with exception: #{exception.message}"
     end
     render template: 'errors/404', status: 404
   end
 
-  def render_500(exception = nil)
+  def render_internal_server_error(exception = nil)
     if exception
       logger.info "Rendering 500 with exception: #{exception.message}"
     end
