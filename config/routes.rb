22c844d60a65ca023c5c436d675c067573e0f521
ta-chibana <takuma.chibana314@gmail.com>
2015-12-29 23:59:11 +0900

    Add user_profiles view for new and edit

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
   root to: 'sample#index'
 
   resources 'monthly_reports', except: :destroy
+  resources 'user_profiles', only: [:show, :new, :create, :edit, :update]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
