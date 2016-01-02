43977a42e5d7188c1e41c81ab7b5b800cdc35821
ta-chibana <takuma.chibana314@gmail.com>
2016-01-03 00:17:33 +0900

    Changed to dynamically generate form and deleted some helper methods

--- app/helpers/user_profiles_helper.rb
+++ app/helpers/user_profiles_helper.rb
@@ -1,37 +1,18 @@
 module UserProfilesHelper
-  def male?(profile)
-    profile.gender == 'male'
-  end
-
-  def female?(profile)
-    profile.gender == 'female'
-  end
-
-  def gender_hidden?(profile)
-    profile.gender == 'gender_hidden'
-  end
-
-  def blood_a?(profile)
-    profile.blood_type == 'a'
-  end
-
-  def blood_b?(profile)
-    profile.blood_type == 'b'
-  end
-
-  def blood_ab?(profile)
-    profile.blood_type == 'ab'
-  end
-
-  def blood_o?(profile)
-    profile.blood_type == 'o'
-  end
-
-  def blood_hidden?(profile)
-    profile.blood_type == 'blood_hidden'
+  def labels
+    {
+      'gender_hidden' => '秘密',
+      'male'          => '男性',
+      'female'        => '女性',
+      'blood_hidden'  => '秘密',
+      'a'             => 'A型',
+      'b'             => 'B型',
+      'ab'            => 'AB型',
+      'o'             => 'O型'
+    }
   end
 
   def add_class(profile, item)
-    send("#{item}?", profile) ? 'active' : ''
+    profile.send("#{item}?") ? 'active' : ''
   end
 end
