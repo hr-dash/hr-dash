53fdeede712b13656552d0c822e3a02782d39e41
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-01-17 20:45:08 +0900

    Fix rubocop

--- spec/requests/admin/dashboard_spec.rb
+++ spec/requests/admin/dashboard_spec.rb
@@ -3,16 +3,16 @@ describe Admin::DashboardController, type: :request do
   let!(:role) { create :user_role, user: user, admin: admin_flag }
   before { login user }
 
-  describe "can login to active_admin only admin role" do
+  describe 'can login to active_admin only admin role' do
     before { get admin_root_path }
 
-    context "admin user" do
+    context 'admin user' do
       let(:admin_flag) { true }
       it { expect(response).to have_http_status :success }
       it { expect(response.body).to match 'ダッシュボード' }
     end
 
-    context "not admin user" do
+    context 'not admin user' do
       let(:admin_flag) { false }
       it { expect(response).to have_http_status :redirect }
       it { expect(response.body).not_to match 'ダッシュボード' }
