74f6c720ec055d88785010661b0267c637a4c3ae
ta-chibana <takuma.chibana314@gmail.com>
2016-01-27 00:09:48 +0900

    Add before filter and add spec

--- spec/requests/user_profile_spec.rb
+++ spec/requests/user_profile_spec.rb
@@ -10,8 +10,38 @@ describe UserProfilesController, type: :request do
   end
 
   describe '#edit GET /user_profiles/:id/edit' do
-    before { get edit_user_profile_path(id: profile.user.id) }
-    it { expect(response).to have_http_status :success }
-    it { expect(response).to render_template 'user_profiles/edit' }
+    let(:user_id) { profile.user_id }
+
+    context 'valid' do
+      before do
+        login profile.user
+        get edit_user_profile_path(id: user_id)
+      end
+
+      it { expect(response).to have_http_status :success }
+      it { expect(response).to render_template 'user_profiles/edit' }
+    end
+
+    context 'invalid' do
+      let(:invalid_user_id) { user_id + 1 }
+      before do
+        login profile.user
+        get edit_user_profile_path(id: invalid_user_id)
+      end
+
+      it { expect(response).to have_http_status :redirect }
+      it { expect(response).to redirect_to root_path }
+    end
+
+    context 'not found' do
+      let(:user) { create :user }
+      before do
+        login user
+        get edit_user_profile_path(id: user.id)
+      end
+
+      it { expect(response).to have_http_status :not_found }
+      it { expect(response).to render_template 'errors/404' }
+    end
   end
 end
