42c3e0f54685a17e8a9839c34d5c1906c7cc1484
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-01-26 20:32:24 +0900

    Display admin link only admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,6 @@ Tag.destroy_all
 
 user = FactoryGirl.create(:user, email: 'test@example.com', password: 'password')
 FactoryGirl.create(:user_role, :admin, user: user)
+FactoryGirl.create(:user, email: 'testuser@example.com', password: 'password')
 
 5.times { FactoryGirl.create(:monthly_report_tag) }
