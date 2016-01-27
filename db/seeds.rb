cd467ac0b955afa0a5fc45280b501aa0b1ced2d9
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-01-27 19:16:16 +0900

    Edit seed for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,14 @@ User.destroy_all
 MonthlyReport.destroy_all
 Tag.destroy_all
 
-user = FactoryGirl.create(:user, email: 'test@example.com', password: 'password')
-FactoryGirl.create(:user_role, :admin, user: user)
-FactoryGirl.create(:user, email: 'testuser@example.com', password: 'password')
+if Rails.env.development?
+  user = FactoryGirl.create(:user, email: 'test@example.com', password: 'password')
+  FactoryGirl.create(:user_role, :admin, user: user)
 
-5.times { FactoryGirl.create(:monthly_report_tag) }
+  FactoryGirl.create(:user, email: 'testuser@example.com', password: 'password')
+
+  5.times { FactoryGirl.create(:monthly_report_tag) }
+else
+  user = User.create(name: 'admin', email: 'test@example.com', password: 'password', employee_code: 'x', entry_date: Date.today, beginner_flg: true)
+  UserRole.create(user: user, admin: true)
+end
