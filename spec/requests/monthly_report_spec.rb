388ab5ebbde9523a1bc378af7384b720ed7a9b9e
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-02-04 22:50:26 +0900

    Use single-quote

--- spec/requests/monthly_report_spec.rb
+++ spec/requests/monthly_report_spec.rb
@@ -10,7 +10,7 @@ describe MonthlyReportsController, type: :request do
     it { expect(response.body).to match report.user.name }
   end
 
-  describe "root_path" do
+  describe 'root_path' do
     before { get root_path }
     it { expect(response).to have_http_status :success }
     it { expect(response).to render_template('monthly_reports/index') }
