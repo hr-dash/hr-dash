b028f70a510e253def74e441559aec94e247ba0d
Satoshi Kuwahara <r4nhhde37t@gmail.com>
2016-03-13 00:42:24 +0900

    display mail line break

--- app/mailers/notify.rb
+++ app/mailers/notify.rb
@@ -8,7 +8,7 @@ class Notify < ApplicationMailer
   def monthly_report_registration(user_id,monthly_report_id)
     @report = MonthlyReport.find(monthly_report_id)
     @user = User.find(user_id)
-    @title = "#{@user.name}が#{@report.created_at.strftime('%Y年%m月')}の月報を登録しました"
+    @title = "#{@user.name}が#{@report.target_month.strftime('%Y年%m月')}の月報を登録しました"
     mail(to: @user.email, subject: @title)
   end
 end
