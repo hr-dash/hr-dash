6e36ad0bc80e94589da4caff72d2408410d64b7f
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-03-15 22:54:18 +0900

    Add flash_messages view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def markdown(text)
     GitHub::Markdown.render_gfm(h(text))
   end
+
+  def alert_class_for(flash_type)
+    types = {
+      success: 'alert-success',
+      notice: 'alert-info',
+      alert: 'alert-warning',
+      error: 'alert-danger',
+    }
+    types[flash_type.to_sym]
+  end
 end
