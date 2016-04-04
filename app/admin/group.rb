04493e82b0b119e6836642147402d24f738d1612
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-04-04 21:58:31 +0900

    Customize group in Admin
    - disable :destory action
    - show group member
    - use datepicker for deleted_at

--- app/admin/group.rb
+++ app/admin/group.rb
@@ -1,4 +1,31 @@
 ActiveAdmin.register Group do
   csv_importable
   permit_params { Group.column_names }
+  actions :all, except: [:destroy]
+
+  show do
+    attributes_table do
+      row :id
+      row :name
+      row :description
+      row 'メンバー' do |group|
+        group.users.each do |user|
+          div { user.name }
+        end
+      end
+      row :deleted_at
+      row :created_at
+      row :updated_at
+    end
+  end
+
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      f.input :name
+      f.input :description
+      f.input :deleted_at, as: :datepicker
+    end
+    f.actions
+  end
 end
