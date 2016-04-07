c185239ca2e69342b9bbfa0c411f0eeb2ca1f5ee
ta-chibana <takuma.chibana314@gmail.com>
2016-04-07 21:37:44 +0900

    Deleted UserProfile#new

--- spec/requests/user_profile_spec.rb
+++ spec/requests/user_profile_spec.rb
@@ -46,13 +46,6 @@ describe UserProfilesController, type: :request do
     end
   end
 
-  # describe '#new GET /user_profiles/new' do
-  #   before { get new_user_profile_path }
-  #   it { expect(response).to have_http_status :success }
-  #   it { expect(response).to render_template('user_profiles/new') }
-  #   it { expect(response.body).to match profile.user.name }
-  # end
-
   describe '#edit GET /user_profiles/:id/edit' do
     let(:user_id) { profile.user_id }
 
