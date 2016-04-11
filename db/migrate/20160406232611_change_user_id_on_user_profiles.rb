dd5c7d8739f086a0f8a79302c29adcbf1d965d8e
ta-chibana <takuma.chibana314@gmail.com>
2016-04-11 19:42:36 +0900

    Added unique index

--- db/migrate/20160406232611_change_user_id_on_user_profiles.rb
+++ db/migrate/20160406232611_change_user_id_on_user_profiles.rb
@@ -1,5 +1,7 @@
 class ChangeUserIdOnUserProfiles < ActiveRecord::Migration
   def change
-    change_column(:user_profiles, :user_id, :integer, null: false, unique: true)
+    remove_index :user_profiles, :user_id
+    add_index :user_profiles, :user_id, unique: true 
+    change_column :user_profiles, :user_id, :integer, null: false
   end
 end
