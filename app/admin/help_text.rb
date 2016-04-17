00b6c9d3f49f9d2407988f493dab153c90e4fb9f
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-04-17 06:46:35 +0000

    Add admin for HelpText

---
+++ app/admin/help_text.rb
@@ -0,0 +1,28 @@
+ActiveAdmin.register HelpText do
+  active_admin_action_log
+  permit_params { HelpText.column_names }
+  actions :all, except: [:destroy]
+
+  index do
+    id_column
+    column :category
+    column :help_type
+    column :target
+    column :body
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :category
+      row :help_type
+      row :target
+      row :body do |help_text|
+        pre { help_text.body }
+      end
+      row :created_at
+      row :updated_at
+    end
+  end
+end
