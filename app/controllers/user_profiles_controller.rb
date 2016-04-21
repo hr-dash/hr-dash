ea73c6bb122859a88791ce248b1e9317104ae280
ta-chibana <takuma.chibana314@gmail.com>
2016-04-22 00:48:51 +0900

    Removed before_action

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -1,16 +1,14 @@
 class UserProfilesController < ApplicationController
-  before_action :correct_user!, only: [:edit, :update]
-
   def show
     @profile = UserProfile.find(params[:id])
   end
 
   def edit
-    @profile = UserProfile.find(params[:id])
+    @profile = UserProfile.find_by!(id: params[:id], user_id: current_user.id)
   end
 
   def update
-    @profile = UserProfile.find(params[:id])
+    @profile = UserProfile.find_by!(id: params[:id], user_id: current_user.id)
     if @profile.update(permitted_params)
       redirect_to @profile
     else
@@ -23,10 +21,5 @@ class UserProfilesController < ApplicationController
   def permitted_params
     params.require(:user_profile)
       .permit(:self_introduction, :gender, :blood_type, :birthday)
-  end
-
-  def correct_user!
-    profile_id = current_user.user_profile.id
-    redirect_to root_path unless profile_id == params[:id].to_i
   end
 end
