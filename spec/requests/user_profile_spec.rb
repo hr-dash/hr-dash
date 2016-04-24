22103349c1b32489e758177a24172b54258c40e1
ta-chibana <takuma.chibana314@gmail.com>
2016-04-25 01:08:22 +0900

    Refactored user_profile_spec #56
    removed duplicate code and unused code.

--- spec/requests/user_profile_spec.rb
+++ spec/requests/user_profile_spec.rb
@@ -6,56 +6,54 @@ describe UserProfilesController, type: :request do
     before { login user }
 
     context 'valid' do
-      before { get user_profile_path(profile) }
-      it { expect(response).to have_http_status :success }
-      it { expect(response).to render_template 'user_profiles/show' }
-      it { expect(response.body).to match user.name }
-      it { expect(response.body).to match user.group.name }
-      it { expect(response.body).to match user.employee_code.to_s }
-      it { expect(response.body).to match user.email }
-      it { expect(response.body).to match user.entry_date.to_s }
-      it { expect(response.body).to match profile.gender_i18n }
-      it { expect(response.body).to match profile.blood_type_i18n }
-      it { expect(response.body).to match profile.birthday.to_s }
-      it { expect(response.body).to match 'プロフィール編集' }
-    end
-
-    context 'valid login user except profile owner' do
-      let(:other_user) { create :user }
-      let(:other_user_profile) { other_user.user_profile }
-      before do
-        login other_user
-        get user_profile_path(profile)
-      end
-      it { expect(response).to have_http_status :success }
-      it { expect(response).to render_template 'user_profiles/show' }
-      it { expect(response.body).to match user.name }
-      it { expect(response.body).to match user.group.name }
-      it { expect(response.body).to match user.employee_code.to_s }
-      it { expect(response.body).to match user.email }
-      it { expect(response.body).to match user.entry_date.to_s }
-      it { expect(response.body).to match profile.gender_i18n }
-      it { expect(response.body).to match profile.blood_type_i18n }
-      it { expect(response.body).to match profile.birthday.to_s }
-      it { expect(response.body).not_to match 'プロフィール編集' }
-    end
-
-    context 'valid without group_id' do
-      let(:user) { create(:user, group_id: nil) }
-      before { get user_profile_path(user.user_profile) }
-      it { expect(response).to have_http_status :success }
-      it { expect(response).to render_template 'user_profiles/show' }
-    end
-
-    context 'valid without birthday' do
-      let(:user) do
-        create(:user) { |u| u.user_profile.birthday = nil }
+      shared_examples 'profile is displayed' do
+        it { expect(response).to have_http_status :success }
+        it { expect(response).to render_template 'user_profiles/show' }
+        it { expect(response.body).to match user.name }
+        it { expect(response.body).to match user.group.name }
+        it { expect(response.body).to match user.employee_code.to_s }
+        it { expect(response.body).to match user.email }
+        it { expect(response.body).to match user.entry_date.to_s }
+        it { expect(response.body).to match profile.gender_i18n }
+        it { expect(response.body).to match profile.blood_type_i18n }
+        it { expect(response.body).to match profile.birthday.to_s }
       end
 
-      before { get user_profile_path(user.user_profile) }
-      it { expect(response).to have_http_status :success }
-      it { expect(response).to render_template 'user_profiles/show' }
+      context 'by owner' do
+        before { get user_profile_path(profile) }
+        it_behaves_like 'profile is displayed'
+        it { expect(response.body).to match 'プロフィール編集' }
+      end
+
+      context 'by other user' do
+        let(:other_user) { create :user }
+        before do
+          login other_user
+          get user_profile_path(profile)
+        end
+
+        it_behaves_like 'profile is displayed'
+        it { expect(response.body).not_to match 'プロフィール編集' }
+      end
+
+      context 'without group_id' do
+        let(:user) { create(:user, group_id: nil) }
+        before { get user_profile_path(user.user_profile) }
+        it { expect(response).to have_http_status :success }
+        it { expect(response).to render_template 'user_profiles/show' }
+      end
+
+      context 'without birthday' do
+        let(:user) do
+          create(:user) { |u| u.user_profile.birthday = nil }
+        end
+
+        before { get user_profile_path(user.user_profile) }
+        it { expect(response).to have_http_status :success }
+        it { expect(response).to render_template 'user_profiles/show' }
+      end
     end
+
 
     context 'not_found' do
       let(:not_found_id) { 0 }
@@ -79,16 +77,17 @@ describe UserProfilesController, type: :request do
     end
 
     context 'invalid' do
-      let(:other_user) { create(:user) }
-      before do
-        login other_user
-        get edit_user_profile_path(profile)
+      context 'by other user' do
+        let(:other_user) { create(:user) }
+        before do
+          login other_user
+          get edit_user_profile_path(profile)
+        end
+
+        it { expect(response).to have_http_status :not_found }
       end
 
-      it { expect(response).to have_http_status :not_found }
-    end
-
-    context 'not found' do
+      context 'profile does not exist'
       let(:profile_id) { 0 }
       before do
         login profile.user
@@ -115,23 +114,25 @@ describe UserProfilesController, type: :request do
     end
 
     context 'invalid' do
-      let(:other_user) { create(:user) }
-      before do
-        login other_user
-        patch user_profile_path(profile, patch_params)
+      context 'by other user' do
+        let(:other_user) { create(:user) }
+        before do
+          login other_user
+          patch user_profile_path(profile, patch_params)
+        end
+
+        it { expect(response).to have_http_status :not_found }
       end
 
-      it { expect(response).to have_http_status :not_found }
-    end
+      context 'profile does not exist' do
+        let(:profile_id) { 0 }
+        before do
+          login profile.user
+          patch user_profile_path(profile_id, patch_params)
+        end
 
-    context 'not_found' do
-      let(:profile_id) { 0 }
-      before do
-        login profile.user
-        patch user_profile_path(profile_id, patch_params)
+        it { expect(response).to have_http_status :not_found }
       end
-
-      it { expect(response).to have_http_status :not_found }
     end
   end
 end
