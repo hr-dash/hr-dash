317e8f4813723291e547618c2f83ca7595fcabe1
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-05-25 00:35:58 +0900

    Setup peek

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+// Peek
+//= require peek
+//= require peek/views/rblineprof
+
 //= require jquery
 //= require jquery_ujs
 
