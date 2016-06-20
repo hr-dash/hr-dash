0c6648dede5835fae81fc3c67a5fba36a137b2da
“nkudo” <kudo_27nov@yahoo.co.jp>
2016-06-20 23:37:57 +0900

    add update in the incorrect value to user_profile_spec

--- spec/requests/user_profile_spec.rb
+++ spec/requests/user_profile_spec.rb
@@ -104,11 +104,22 @@ describe UserProfilesController, type: :request do
     context 'valid' do
       before do
         login profile.user
-        patch user_profile_path(profile, patch_params)
       end
 
-      it { expect(response).to have_http_status :redirect }
-      it { expect(response).to redirect_to user_profile_path(user.user_profile) }
+      context 'valid_params' do
+        before { patch user_profile_path(profile, patch_params) }
+
+        it { expect(response).to have_http_status :redirect }
+        it { expect(response).to redirect_to user_profile_path(user.user_profile) }
+      end
+
+      context 'invalid_params' do
+        let(:invalid_params) { patch_params.merge(blood_type: 'test') }
+        before { patch user_profile_path(profile, invalid_params) }
+
+        it { expect(response).to have_http_status :redirect }
+        it { expect(response).to redirect_to user_profile_path(user.user_profile) }
+      end
     end
 
     context 'invalid' do
