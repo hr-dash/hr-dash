cbd031287c1134fd6f1aae7be5569a16b616a71b
ta-chibana <takuma.chibana314@gmail.com>
2016-06-25 15:34:42 +0900

    Fix n + 1 problem on monthly_reports_controller

--- app/controllers/monthly_reports_controller.rb
+++ app/controllers/monthly_reports_controller.rb
@@ -84,7 +84,7 @@ class MonthlyReportsController < ApplicationController
   end
 
   def user_reports_in_year(year, report_user)
-    reports = MonthlyReport.year(year).where(user: report_user)
+    reports = MonthlyReport.year(year).where(user: report_user).includes(:user)
 
     (1..12).map do |month|
       target_month = Date.new(year, month, 1)
