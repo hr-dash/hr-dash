cf77234102f043a22f6c92c02556c029a5f10223
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-07-06 02:18:38 +0900

    Add admin user_role spec

---
+++ spec/features/admin/user_role_spec.rb
@@ -0,0 +1,45 @@
+describe 'Admin::UserRole', type: :feature do
+  before { login(admin: true) }
+  let(:user) { create(:user) }
+  let!(:role) { create(:user_role, :admin, user: user) }
+  let(:page_title) { find('#page_title') }
+
+  describe '#index' do
+    before { visit admin_user_roles_path }
+    it { expect(page_title).to have_content('ロール') }
+    it { expect(page).to have_content(user.name) }
+    it { expect(page).to have_content('作成する') }
+    it { expect(page).to have_css('.delete_link') }
+  end
+
+  describe '#show' do
+    before { visit admin_user_role_path(role) }
+    it { expect(page).to have_content(user.name) }
+  end
+
+  describe '#create' do
+    let!(:new_user) { create(:user) }
+    before do
+      visit new_admin_user_role_path
+      select new_user.name, from: 'user_role_user_id'
+      check 'user_role_admin'
+      click_on 'Create ロール'
+    end
+
+    it { expect(page).to have_content(new_user.name) }
+    it { expect(current_path).to eq admin_user_role_path(new_user.role) }
+  end
+
+  describe '#destroy', js: true do
+    before do
+      visit admin_user_roles_path
+      accept_confirm do
+        find("#user_role_#{role.id}").find('.delete_link').click
+      end
+    end
+
+    it { expect(current_path).to eq admin_user_roles_path }
+    it { expect(page).not_to have_content(user.name) }
+    it { expect { role.reload }.to raise_error(ActiveRecord::RecordNotFound) }
+  end
+end
