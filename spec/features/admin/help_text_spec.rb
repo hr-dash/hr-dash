1793348b4b626580e05df54dc638df2dc7407883
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-07-09 00:51:45 +0900

    Add admin help_text spec

---
+++ spec/features/admin/help_text_spec.rb
@@ -0,0 +1,35 @@
+describe 'Admin::HelpText', type: :feature do
+  before { login(admin: true) }
+  let!(:help_text) { create(:help_text) }
+  let(:page_title) { find('#page_title') }
+
+  describe '#index' do
+    before { visit admin_help_texts_path }
+    it { expect(page_title).to have_content('ヘルプテキスト') }
+    it { expect(page).to have_content(help_text.category) }
+    it { expect(page).not_to have_content('作成する') }
+    it { expect(page).not_to have_css('.delete_link') }
+  end
+
+  describe '#show' do
+    before { visit admin_help_text_path(help_text) }
+    it { expect(page_title).to have_content("##{help_text.id}") }
+    it { expect(page).to have_content(help_text.category) }
+    it { expect(page).to have_content(help_text.target) }
+    it { expect(page).to have_content(help_text.body) }
+  end
+
+  describe '#update' do
+    let(:new_body) { Faker::Lorem.paragraph }
+    before do
+      visit edit_admin_help_text_path(help_text)
+      fill_in 'テキスト', with: new_body
+      click_on 'Update ヘルプテキスト'
+    end
+
+    it { expect(page_title).to have_content("##{help_text.id}") }
+    it { expect(current_path).to eq admin_help_text_path(help_text) }
+    it { expect(page).to have_content(new_body) }
+    it { expect(help_text.reload.body).to eq new_body }
+  end
+end
