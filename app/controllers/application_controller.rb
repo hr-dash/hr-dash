03ddab1689fa368e33102e4af440aef54de6dace
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-07-25 23:12:08 +0900

    Use ENV vars

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth_for_admin
     authenticate_or_request_with_http_basic do |username, password|
-      username == "admin" && password == "password"
+      username == ENV['BASIC_AUTH_USERNAME'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 end
