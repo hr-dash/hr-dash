4bc531957aee26b6a492455a977aec4be34d6371
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-08-09 02:01:48 +0900

    Add create action for password_reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,7 +3,16 @@ class PasswordResetsController < Devise::PasswordsController
 
   def new; end
 
-  def create; end
+  def create
+    user = User.find_by(email: params["email"])
+
+    if user&.active_for_authentication?
+      user.send_reset_password_instructions
+    else
+      flash[:error] = '有効なメールアドレスではありません。'
+      redirect_to new_password_reset_path
+    end
+  end
 
   def edit; end
 
---
+++ app/mailers/devise_mailer.rb
@@ -0,0 +1,2 @@
+class DeviseMailer < Devise::Mailer
+end
