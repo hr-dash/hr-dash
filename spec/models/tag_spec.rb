8d45d0599f051911bc229e45ea5ceed608fdf851
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-09-09 01:49:55 +0900

    Add validation for format of tag name

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -17,5 +17,33 @@ describe Tag, type: :model do
     it { is_expected.to validate_presence_of(:status) }
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_length_of(:name).is_at_most(32) }
+
+    describe 'VALID_NAME_REGEX' do
+      context 'allowed chars' do
+        allowed_chars = [
+          'a', 'A', '0',
+          '_', '.', '+',
+          '#', "'", '-',
+          'spa ce', 'あ', 'ア', '漢'
+        ]
+
+        allowed_chars.each do |c|
+          it { is_expected.to allow_value(c).for(:name) }
+        end
+      end
+
+      context 'not allowed chars' do
+        not_allowed_chars = [
+          '!', '"', '$', '%', '&',
+          '(', ')', '=', '~', '?',
+          '|', '@', '`', '{', '}',
+          '*', ';', ',', ':', '/'
+        ]
+
+        not_allowed_chars.each do |c|
+          it { is_expected.not_to allow_value(c).for(:name) }
+        end
+      end
+    end
   end
 end
