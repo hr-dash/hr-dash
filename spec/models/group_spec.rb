546c2195af46811e182c5814f8a6d17cf8bc5561
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-09-17 02:07:54 +0900

    Add test for .active scope

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -19,4 +19,24 @@ describe Group, type: :model do
   describe 'Validations' do
     it { is_expected.to validate_presence_of(:name) }
   end
+
+  describe '.active' do
+    let!(:group) { create(:group, deleted_at: deleted_at) }
+    subject { described_class.active.present? }
+
+    context 'when deleted_at is nil' do
+      let(:deleted_at) { nil }
+      it { is_expected.to eq true }
+    end
+
+    context 'when deleted_at is future' do
+      let(:deleted_at) { Time.current.tomorrow }
+      it { is_expected.to eq true }
+    end
+
+    context 'when deleted_at is past' do
+      let(:deleted_at) { Time.current.yesterday }
+      it { is_expected.to eq false }
+    end
+  end
 end
