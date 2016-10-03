121c128f44cb3ae602c55b7b714eec9cf2b18fef
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-10-03 16:04:44 +0900

    Add annotate for user model(columns for account lock)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,9 @@
 #  last_sign_in_at        :datetime
 #  current_sign_in_ip     :inet
 #  last_sign_in_ip        :inet
+#  failed_attempts        :integer          default(0), not null
+#  unlock_token           :string
+#  locked_at              :datetime
 #  gender                 :integer          default(0), not null
 #
 
