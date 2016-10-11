d6424fdabe37542940d47b02344254b72b98e9df
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-10-12 02:02:43 +0900

    Add attrs to inquiry factory

--- spec/factories/inquiries.rb
+++ spec/factories/inquiries.rb
@@ -2,5 +2,8 @@ FactoryGirl.define do
   factory :inquiry do
     association :user
     body { Faker::Lorem.paragraph }
+    referer { Faker::Internet.url }
+    user_agent { Faker::Lorem.sentence }
+    session_id { Faker::Lorem.characters(32) }
   end
 end
