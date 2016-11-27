7735b096b8d21b0b4aa364733c1bd5afebe1dd02
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-11-28 00:36:10 +0900

    Fix rubocop

--- spec/tasks/tag_cleaner_spec.rb
+++ spec/tasks/tag_cleaner_spec.rb
@@ -13,7 +13,7 @@ describe 'tag_cleaner' do
     end
 
     context 'exec delete' do
-      before { Rake::Task['tag_cleaner:delete_unused_tags'].invoke "true" }
+      before { Rake::Task['tag_cleaner:delete_unused_tags'].invoke 'true' }
       it { expect(used_tag.reload).to be_present }
       it { expect { unused_tag.reload }.to raise_error(ActiveRecord::RecordNotFound) }
       it { expect(fixed_tag.reload).to be_present }
