e2ae6af2360870d76f4ce3757882b4f78c78ffb0
tomoyuki-tanaka <masuminium5@gmail.com>
2016-12-11 21:26:14 +0000

    dev/356 remove mailer/end_of_month

--- app/mailers/mailer/end_of_month.rb
+++
@@ -1,9 +0,0 @@
-module Mailer
-  class EndOfMonth < ApplicationMailer
-    def notice
-      @domain = ENV['MAILER_DEFAULT_HOST']
-      title = '[お知らせ]今月の月報が登録可能になりました。'
-      mail(to: ENV['MAILING_LIST_TO_ALL_USER'], subject: title)
-    end
-  end
-end
