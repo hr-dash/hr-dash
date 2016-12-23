66df72ccd256f823dd7c9f079ef01f2f36e891b8
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-12-24 00:10:32 +0900

    Add friendly forwarding

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,20 @@
 class SessionsController < Devise::SessionsController
   layout false
 
-  before_action :reset_session, only: :create
+  before_action :reset_session_before_login, only: :create
 
   def password_reset
     reset_session
     redirect_to new_password_reset_path
+  end
+
+  private
+
+  def reset_session_before_login
+    user_return_to = session[:user_return_to]
+    reset_session
+
+    # friendly forwarding
+    session[:user_return_to] = user_return_to if user_return_to
   end
 end
