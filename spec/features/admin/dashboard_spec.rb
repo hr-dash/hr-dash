3af275b139da93b61c6e8257ddc81f7bb5cc8818
ta-chibana <tachibana.gmc@gmail.com>
2016-12-30 21:58:31 +0900

    Add unfixed tags list on dashboard

--- spec/features/admin/dashboard_spec.rb
+++ spec/features/admin/dashboard_spec.rb
@@ -7,5 +7,19 @@ describe 'Admin::Dashboard', type: :feature do
     before { visit admin_root_path }
     let(:title) { find('#page_title') }
     it { expect(title).to have_content('ダッシュボード') }
+    it { expect(page).to have_content('未登録タグ') }
+
+    context 'unfixed tag' do
+      let!(:tag) { create(:tag, :unfixed) }
+      before { visit admin_root_path }
+      it { expect(page).to have_content(tag.name) }
+    end
+
+    context 'fixed tag' do
+      let!(:tag) { create(:tag, :fixed) }
+      before { visit admin_root_path }
+      it { expect(page).not_to have_content(tag.name) }
+      it { expect(page).to have_content('未登録のタグはありません') }
+    end
   end
 end
