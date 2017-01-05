ef01c282b960bf04b2c7efeb17ea3db65b4dadca
ta-chibana <tachibana.gmc@gmail.com>
2017-01-05 21:37:04 +0900

    Add monthly report by groups to dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,13 +1,36 @@
 ActiveAdmin.register_page 'Dashboard' do
+  DISPLAY_MONTHLY_REPORT_LIMIT = 10
+
   menu priority: 1, label: proc { I18n.t('active_admin.dashboard') }
 
   content title: proc { I18n.t('active_admin.dashboard') } do
     columns do
+      Group.active.each do |group|
+        column max_width: '45%', min_width: '45%' do
+          panel "最新の月報 【#{group.name}グループ】" do
+            reports = MonthlyReport
+              .includes(:user)
+              .where(user_id: group.users)
+              .order(created_at: :desc)
+              .limit(DISPLAY_MONTHLY_REPORT_LIMIT)
+
+            table_for reports do
+              column(:id) { |r| link_to(r.id, admin_monthly_report_path(r)) }
+              column(:user)
+              column(:target_month) { |r| r.target_month.strftime('%Y年%m月') }
+              column(:shipped_at) { |r| r.shipped_at.to_s }
+            end
+          end
+        end
+      end
+    end
+
+    columns do
       column do
         panel '未登録タグ', id: :unfixed_tags do
-          unfixed_tags = Tag.unfixed
+          unfixed_tags = Tag.unfixed.order(created_at: :desc)
           if unfixed_tags.present?
-            table_for(unfixed_tags.order(created_at: :desc)) do
+            table_for unfixed_tags do
               column(:name) { |t| link_to(t.name, admin_tag_path(t)) }
             end
           else
@@ -16,5 +39,5 @@ ActiveAdmin.register_page 'Dashboard' do
         end
       end
     end
-  end # content
+  end
 end
--- spec/features/admin/dashboard_spec.rb
+++ spec/features/admin/dashboard_spec.rb
@@ -9,6 +9,16 @@ describe 'Admin::Dashboard', type: :feature do
     it { expect(title).to have_content('ダッシュボード') }
     it { expect(page).to have_content('未登録タグ') }
 
+    context 'monthly_reports' do
+      let!(:report) { create(:monthly_report) }
+      let(:target_month) { report.target_month.strftime('%Y年%m月') }
+      before { visit admin_root_path }
+      it { expect(page).to have_content(report.id) }
+      it { expect(page).to have_content(report.user.name) }
+      it { expect(page).to have_content(target_month) }
+      it { expect(page).to have_content(report.shipped_at.to_s) }
+    end
+
     context 'unfixed tag' do
       let!(:tag) { create(:tag, :unfixed) }
       before { visit admin_root_path }
