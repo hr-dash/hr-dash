17ff7adb60e827e55646742c46f8e47635a5fe88
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2017-01-08 01:23:16 +0900

    Add annotate

--- spec/factories/group_assignments.rb
+++ spec/factories/group_assignments.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: group_assignments
+#
+#  id         :integer          not null, primary key
+#  group_id   :integer          not null
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryGirl.define do
   factory :group_assignment do
     association :group
