12479666fe3f4b512dfc3fdf92c6ec86efadbb8c
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2017-01-07 16:55:47 +0900

    Add notification for monthly_report commented

--- app/mailers/mailer/notify.rb
+++ app/mailers/mailer/notify.rb
@@ -8,5 +8,18 @@ module Mailer
       @title = "#{@user.name}が#{@report.target_month.strftime('%Y年%m月')}の月報を登録しました"
       mail(to: @user.groups.map(&:email), subject: mail_subject(@title))
     end
+
+    def monthly_report_commented(comment_id)
+      @comment = MonthlyReportComment.find(comment_id)
+      report = @comment.monthly_report
+
+      target_users = report.related_users - [@comment.user]
+      return if target_users.blank?
+
+      report_name = "#{report.user.name}さんの月報（#{report.target_month.strftime('%Y年%m月')}）"
+      @title = "#{@comment.user.name}さんが#{report_name}にコメントしました"
+
+      mail(to: target_users.map(&:email), subject: mail_subject(@title))
+    end
   end
 end
