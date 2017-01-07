b2a00ee69cbfbc62cd9dea759bdef5c4392c72cf
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2017-01-08 01:22:43 +0900

    Add test for rake task

---
+++ spec/tasks/comments_counter_spec.rb
@@ -0,0 +1,14 @@
+describe 'comments_counter' do
+  let(:report) { create(:monthly_report, :with_comments, comment_size: comment_size) }
+  let(:comment_size) { Faker::Number.between(1, 10) }
+
+  before do
+    # set wrong count
+    report.update(comments_count: 0)
+    Rake::Task['comments_counter:calculate'].invoke
+  end
+
+  describe 'calculate' do
+    it { expect(report.reload.comments_count).to eq comment_size }
+  end
+end
