ea84288bc829d555ad7451fa966801d95b5ecc2b
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2017-01-21 18:20:05 +0900

    Add errors and rescue action, view

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,14 +2,33 @@ class ErrorsController < ActionController::Base
   layout 'error'
 
   rescue_from StandardError, with: :render_internal_server_error
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActionController::NotImplemented, with: :render_not_implemented
   rescue_from ActionController::RoutingError, with: :render_not_found
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActionController::MethodNotAllowed, with: :render_method_not_allowed
+  rescue_from ActiveRecord::StaleObjectError, with: :render_conflict
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+  rescue_from ActiveRecord::RecordNotSaved, with: :render_unprocessable_entity
+  rescue_from ActionController::InvalidAuthenticityToken, with: :render_unprocessable_entity
 
   def render_not_found(exception = nil)
-    if exception
-      logger.info "Rendering 404 with exception: #{exception.message}"
-    end
+    log_error(exception)
     render template: 'errors/404', status: 404
+  end
+
+  def render_method_not_allowed(exception = nil)
+    log_error(exception)
+    render template: 'errors/405', status: 405
+  end
+
+  def render_conflict(exception = nil)
+    log_error(exception)
+    render template: 'errors/409', status: 409
+  end
+
+  def render_unprocessable_entity(exception = nil)
+    log_error(exception)
+    render template: 'errors/422', status: 422
   end
 
   def render_internal_server_error(exception = nil)
@@ -20,7 +39,22 @@ class ErrorsController < ActionController::Base
     render template: 'errors/500', status: 500
   end
 
+  def render_not_implemented(exception = nil)
+    if exception
+      Mailer::Error.create(exception, current_user, request).deliver_now
+      logger.info "Rendering 501 with exception: #{exception.message}"
+    end
+    render template: 'errors/501', status: 501
+  end
+
   def show
     fail env['action_dispatch.exception']
+  end
+
+  private
+
+  def log_error(exception)
+    return unless exception
+    logger.info "#{exception.class} error has occurred with message: #{exception.message}"
   end
 end
