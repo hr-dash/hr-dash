60e4c5f817a1e438e9c92110effcf0871de37727
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2017-01-24 00:05:34 +0900

    Display only latest reports on top page

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,7 +1,7 @@
 class RootController < ApplicationController
   def index
     references = [{ user: :groups }, :monthly_working_process, { monthly_report_tags: :tag }]
-    @monthly_reports = MonthlyReport.includes(references).released.order('shipped_at desc').first(5)
+    @monthly_reports = MonthlyReport.includes(references).latest.released.order('shipped_at desc').first(5)
     @announcements = Announcement.published.first(5)
   end
 end
