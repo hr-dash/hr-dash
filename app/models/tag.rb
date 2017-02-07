e548b85b3dc2ad86ad38f8579b9d3710e0b4167a
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2017-02-08 00:38:49 +0900

    Add status ignored to Tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -15,7 +15,7 @@ class Tag < ActiveRecord::Base
   validates :status, presence: true
   validates :name, length: { maximum: 32 }, presence: true, format: { with: VALID_NAME_REGEX }
 
-  enum status: { unfixed: 0, fixed: 1 }
+  enum status: { unfixed: 0, fixed: 1, ignored: 2 }
 
   def self.find_or_initialize_by_name_ignore_case(name)
     Tag.where('LOWER(name) = LOWER(?)', name).first || Tag.new(name: name)
