9ea3581aef5172794ea17620c898c0799edafb97
tomoyuki-tanaka <masuminium5@gmail.com>
2017-02-09 20:49:37 +0900

    test: add context 'search by tag name'.

--- spec/features/monthly_report_spec.rb
+++ spec/features/monthly_report_spec.rb
@@ -10,6 +10,27 @@ describe MonthlyReportsController, type: :feature do
       before { visit monthly_reports_path }
       it { expect(first_report[:href]).to eq monthly_report_path(today) }
     end
+
+    describe 'search', js: true do
+      context 'by tag name' do
+        let!(:report) { create(:monthly_report, :shipped, :with_tags, tag_size: 1) }
+        let!(:tag) { report.tags.first }
+        let(:url) { URI.parse(current_url) }
+        let(:query) { URI.decode(url.query) }
+
+        before do
+          create_list(:monthly_report, 10, :shipped, tags: report.tags)
+          visit monthly_reports_path
+          find('#monthly_report_tags_input').set(tag.name)
+          click_button '検索'
+          click_link '>'
+        end
+
+        it { expect(current_path).to eq monthly_reports_path }
+        it { expect(find('div.tag > span').text).to eq tag.name }
+        it { expect(query).to include "q[tags_name_in][]=#{tag.name}" }
+      end
+    end
   end
 
   describe '#show GET /monthly_reports/:id' do
