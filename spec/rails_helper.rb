195dfd7c62dd71d75a7a84d822be6b89084bfaf6
tomoyuki-tanaka <masuminium5@gmail.com>
2017-02-19 09:23:37 +0900

    bundle update & rails update

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -84,4 +84,10 @@ RSpec.configure do |config|
   config.after :each do
     DatabaseRewinder.clean
   end
+
+  [:controller, :view, :request].each do |type|
+    config.include ::Rails::Controller::Testing::TestProcess, :type => type
+    config.include ::Rails::Controller::Testing::TemplateAssertions, :type => type
+    config.include ::Rails::Controller::Testing::Integration, :type => type
+  end
 end
