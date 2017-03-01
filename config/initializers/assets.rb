7ce0dd4bc89af7733e91ad31cab188cbc51b3067
tomoyuki-tanaka <masuminium5@gmail.com>
2017-03-02 07:46:43 +0900

    fix:remove unused configs

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += [/^[a-z0-9]\w+.(css|js|woff|eot|svg|ttf)$/]
-# Rails.application.config.assets.precompile += %w( search.js )
+
