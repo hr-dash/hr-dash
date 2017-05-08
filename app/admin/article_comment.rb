be6078e072d1a5374886c8688a6001471bce72e8
tomoyuki-tanaka <masuminium5@gmail.com>
2017-05-08 22:01:20 +0900

    feature: admin page for article_comment and article_tag

---
+++ app/admin/article_comment.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+ActiveAdmin.register ArticleComment do
+  menu parent: '記事'
+  active_admin_action_log
+  permit_params { ArticleComment.column_names }
+  actions :all, except: [:new, :create]
+
+  controller do
+    def scoped_collection
+      super.includes :user, :article
+    end
+  end
+
+  index do
+    selectable_column
+    id_column
+    column :user
+    column :comment
+    column :article
+    column :created_at
+    actions
+  end
+
+  filter :user
+  filter :article_id
+  filter :comment
+
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      f.input :user
+      f.input :article_id
+      f.input :comment
+    end
+    f.actions
+  end
+end
---
+++ app/admin/article_tag.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+ActiveAdmin.register ArticleTag do
+  menu parent: '記事'
+  active_admin_action_log
+  permit_params { ArticleTag.column_names }
+
+  controller do
+    def scoped_collection
+      super.includes :article, :tag
+    end
+  end
+
+  index do
+    selectable_column
+    id_column
+    column :article
+    column :tag
+    actions
+  end
+
+  filter :article_id
+  filter :tag
+
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      f.input :article_id
+      f.input :tag
+    end
+    f.actions
+  end
+end
