35dce3579f26418d35e38591ba66b8d51197f184
tomoyuki-tanaka <masuminium5@gmail.com>
2017-06-11 17:55:16 +0900

    test: add article admin specs

---
+++ spec/features/admin/article_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+describe 'Admin::Article', type: :feature do
+  before { login(user, admin: true) }
+  let(:user) { create(:user) }
+  let!(:article) { create(:article, user: user) }
+  let(:page_title) { find('#page_title') }
+
+  describe '#index' do
+    before { visit admin_articles_path }
+    it 'should open index page.' do
+      expect(page_title).to have_content('記事')
+      expect(page).to have_content(user.name)
+      expect(page).to have_content(article.shipped_at)
+    end
+  end
+
+  describe '#show' do
+    before { visit admin_article_path(article) }
+    it { expect(page).to have_content(article.body) }
+  end
+
+  describe '#update' do
+    let(:new_title) { Faker::Lorem.word }
+    before do
+      visit edit_admin_article_path(article)
+      fill_in 'タイトル', with: new_title
+      click_on '記事を更新'
+    end
+
+    it 'should update the article.' do
+      expect(page).to have_content(new_title)
+      expect(current_path).to eq admin_article_path(article)
+      expect(article.reload.title).to eq new_title
+    end
+  end
+
+  describe '#dastroy', js: true do
+    before do
+      visit admin_articles_path
+      accept_confirm do
+        find("#article_#{article.id}").find('.delete_link').click
+      end
+    end
+
+    it 'should delete the article.' do
+      expect(current_path).to eq admin_articles_path
+      expect(page).not_to have_content(article.title)
+      expect { article.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+end
---
+++ spec/features/admin/article_tag_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+describe 'Admin::ArticleTag', type: :feature do
+  before { login(user, admin: true) }
+  let(:user) { create(:user) }
+  let(:article) { create(:article, user: user) }
+  let(:tag) { article_tag.tag }
+  let!(:article_tag) { create(:article_tag, article: article) }
+  let(:page_title) { find('#page_title') }
+
+  describe '#index' do
+    before { visit admin_article_tags_path }
+
+    it 'should open the index page.' do
+      expect(page_title).to have_content('記事タグ')
+      expect(page).to have_content(tag.name)
+      expect(page).to have_css('.delete_link')
+    end
+  end
+
+  describe '#show' do
+    before { visit admin_article_tag_path(article_tag) }
+    it { expect(page).to have_content(tag.name) }
+  end
+
+  describe '#update' do
+    let!(:new_tag) { create(:tag) }
+    before do
+      visit edit_admin_article_tag_path(article_tag)
+      select new_tag.name, from: 'article_tag_tag_id'
+      click_on '記事タグを更新'
+    end
+
+    it 'should update the article tag.' do
+      expect(page).to have_content(new_tag.name)
+      expect(current_path).to eq admin_article_tag_path(article_tag)
+      expect(article_tag.reload.tag).to eq new_tag
+    end
+  end
+
+  describe '#destroy', js: true do
+    before do
+      visit admin_article_tags_path
+      accept_confirm do
+        find("#article_tag_#{article_tag.id}").find('.delete_link').click
+      end
+    end
+
+    it 'should delete the article tag.' do
+      expect(current_path).to eq admin_article_tags_path
+      expect(page).not_to have_content(tag.name)
+      expect { article_tag.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+end
