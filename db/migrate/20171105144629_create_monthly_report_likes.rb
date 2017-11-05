d2ae777eedcb0c33ba3dd920c13eb0d8515342a2
morihirok <omnimani3@gmail.com>
2017-11-06 00:06:13 +0900

    Create monthly_report_like table

---
+++ db/migrate/20171105144629_create_monthly_report_likes.rb
@@ -0,0 +1,10 @@
+class CreateMonthlyReportLikes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :monthly_report_likes do |t|
+      t.belongs_to :user, null: false
+      t.belongs_to :monthly_report, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303143632) do
+ActiveRecord::Schema.define(version: 20171105144629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,6 +98,15 @@ ActiveRecord::Schema.define(version: 20170303143632) do
     t.datetime "updated_at",        null: false
   end
 
+  create_table "monthly_report_likes", force: :cascade do |t|
+    t.integer  "user_id",           null: false
+    t.integer  "monthly_report_id", null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.index ["monthly_report_id"], name: "index_monthly_report_likes_on_monthly_report_id", using: :btree
+    t.index ["user_id"], name: "index_monthly_report_likes_on_user_id", using: :btree
+  end
+
   create_table "monthly_report_tags", force: :cascade do |t|
     t.integer  "monthly_report_id", null: false
     t.integer  "tag_id",            null: false
@@ -128,10 +137,10 @@ ActiveRecord::Schema.define(version: 20170303143632) do
     t.boolean  "process_operation",      default: false, null: false
     t.boolean  "process_analysis",       default: false, null: false
     t.boolean  "process_training",       default: false, null: false
-    t.boolean  "process_structure",      default: false, null: false
-    t.boolean  "process_trouble",        default: false, null: false
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
+    t.boolean  "process_structure",      default: false, null: false
+    t.boolean  "process_trouble",        default: false, null: false
     t.index ["monthly_report_id"], name: "index_monthly_working_processes_on_monthly_report_id", using: :btree
   end
 
