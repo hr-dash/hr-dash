b095ac61b9e21ff13517667578ac349217aa462d
morihirok <omnimani3@gmail.com>
2017-11-12 04:21:06 +0900

    Add feature test about like (and fine fix)

---
+++ spec/features/monthly_report_like_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+describe MonthlyReportCommentsController, type: :feature do
+  let(:user) { create(:user) }
+  let(:report) { create(:monthly_report, :shipped, :with_tags) }
+
+  before { login user }
+
+  context 'when monthly report is not liked' do
+    before do
+      visit monthly_report_path(report)
+    end
+
+    it 'displays Like! btn', js: true do
+      expect(page).to have_content 'Like!'
+    end
+
+    it 'can change to Liked by clicking the btn', js: true do
+      click_on 'Like!'
+      expect(page).to have_content 'Liked'
+    end
+  end
+
+  context 'when monthly report is liked' do
+    before do
+      report.likes.create(user: user)
+      visit monthly_report_path(report)
+    end
+
+    it 'displays Liked btn', js: true do
+      expect(page).to have_content 'Liked'
+    end
+
+    it 'can change to Like! btn by clicking the btn', js: true do
+      click_on 'Liked'
+      expect(page).to have_content 'Like!'
+    end
+  end
+end
